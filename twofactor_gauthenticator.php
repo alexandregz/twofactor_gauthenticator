alexandregz/twofactor_gauthenticator

commit 954d93317deff9d968bd141dd101e800c2227de0

Use log_dir from config

twofactor_gauthenticator.php
@@ -19,8 +19,8 @@ class twofactor_gauthenticator extends rcube_plugin
 {
 	private $_number_recovery_codes = 4;
 
-        // relative from RC home dir, not plugin directory
-        private $_logs_file = '/logs/log_errors_2FA.txt';
+        // relative to $config['log_dir']
+        private $_logs_file = 'log_errors_2FA.txt';
 	
     function init() 
     {
@@ -542,7 +542,8 @@ class twofactor_gauthenticator extends rcube_plugin
 
         // log error into $_logs_file directory
         private function __logError() {
-                file_put_contents(realpath(".").$this->_logs_file, date("Y-m-d H:i:s")."|".$_SERVER['HTTP_X_FORWARDED_FOR']."|".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND);
+		$_log_dir = $rcmail->config->get('log_dir')
+                file_put_contents($_log_dir.'/'.$this->_logs_file, date("Y-m-d H:i:s")."|".$_SERVER['HTTP_X_FORWARDED_FOR']."|".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND);
         }
 
 }

commit 5c457f2b83e96b1320d0cbed3d7f2d8c90fdf15d

Fix missing ';' and end of line 545
Fixes #178, caused by https://github.com/alexandregz/twofactor_gauthenticator/pull/166/commits/954d93317deff9d968bd141dd101e800c2227de0

twofactor_gauthenticator.php
@@ -542,7 +542,7 @@ class twofactor_gauthenticator extends rcube_plugin
 
         // log error into $_logs_file directory
         private function __logError() {
-		$_log_dir = $rcmail->config->get('log_dir')
+		$_log_dir = $rcmail->config->get('log_dir');
                 file_put_contents($_log_dir.'/'.$this->_logs_file, date("Y-m-d H:i:s")."|".$_SERVER['HTTP_X_FORWARDED_FOR']."|".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND);
         }
 